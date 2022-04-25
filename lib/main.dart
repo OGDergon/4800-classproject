7f38b215437aed63db64203f934be129d6c0d686
Monroe <monroedrysdale@gmail.com>
2022-04-24 20:42:01 -0700

    Update folder structure
    add subfolders and organize lib folder

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'TEST 2'),
+      home: const MyHomePage(title: 'this is a test'),
     );
   }
 }
